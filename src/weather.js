e24f4b57495002c7a9b0ceff11b9aace08cb9c68
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2023-09-15 23:51:15 +0800

    create a method to round off

--- src/weather.js
+++ src/weather.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 
+function roundToOneDecimalPlaces(temp) {
+    return Math.round(temp * 10) / 10;
+}
+
 function Weather() {
     const [weather, setWeather] = useState(null);
     const API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
-    const city = "Tokyo";  // TODO: get value dynamically
+    const city = "Tokyo";  // TODO: get value dynamicallyroundToTwoDecimalPlaces(weather.main.temp - 273.15)
 
     useEffect(() => {
-      console.log(process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY);
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
             .then(response => response.json())
             .then(data => {
@@ -22,8 +25,9 @@ function Weather() {
     return (
         <div>
             <h2>{weather.name}</h2>
-            <p>Temperature: {weather.main?.temp - 273.15}°C</p> {/* convert from kelvin to celsius */}
-            {/* <p>Condition: {weather.weather[0].description}</p> */}
+            <p>Temperature: {roundToOneDecimalPlaces(weather.main.temp - 273.15)}°C</p> 
+            {/* <p>Temperature: {weather.main.temp - 273.15}°C</p> convert from kelvin to celsius */}
+            <p>Condition: {weather.weather[0].description}</p>
         </div>
     );
 }
