d4f134e6ce06bf29459ae62db11c5ae7b0e36d8e
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2023-11-12 22:00:47 +0800

    fix erros

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Hello() {
-    return <h1>Hello, React!</h1>;
+    return <h1>Hello, React!</h1>
 }
 
 export default Hello;
--- src/weather.js
+++ src/weather.js
@@ -7,7 +7,7 @@ function roundToOneDecimalPlaces(temp) {
 function Weather() {
     const [weather, setWeather] = useState(null);
     const API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
-    const city = "Tokyo";  // TODO: get value dynamicallyroundToTwoDecimalPlaces(weather.main.temp - 273.15)
+    let city = "Tokyo";  // TODO: get value dynamicallyroundToTwoDecimalPlaces(weather.main.temp - 273.15)
 
     useEffect(() => {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
